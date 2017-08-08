6ad7a7d8d74d73eee60cff5baaf49d415a921236
sura0113 <sura0113@gmail.com>
2017-08-08 21:22:24 +1000

    WorkingScript

---
+++ diskinfo_csv_tab_output.ps1
@@ -0,0 +1,43 @@
+﻿$servers = Get-Content C:\git\suresh-test-git\servers.txt
+#Run the commands for each server in the list
+$infoColl = @()
+Foreach ($s in $servers)
+{
+	$CPUInfo = Get-WmiObject Win32_Processor -ComputerName $s #Get CPU Information
+	$OSInfo = Get-WmiObject Win32_OperatingSystem -ComputerName $s #Get OS Information
+	#Get Memory Information. The data will be shown in a table as MB, rounded to the nearest second decimal.
+	$OSTotalVirtualMemory = [math]::round($OSInfo.TotalVirtualMemorySize / 1MB, 2)
+	$OSTotalVisibleMemory = [math]::round(($OSInfo.TotalVisibleMemorySize / 1MB), 2)
+	$PhysicalMemory = Get-WmiObject CIM_PhysicalMemory -ComputerName $s | Measure-Object -Property capacity -Sum | % { [Math]::Round(($_.sum / 1GB), 2) }
+    $Pagefilesize=get-wmiobject -ComputerName $s Win32_pagefileusage | % {$_.AllocatedBaseSize} 
+    $network = Get-WmiObject Win32_NetworkAdapterConfiguration -EA Stop | ? {$_.IPEnabled} 
+        foreach ($Network in $Networks) {            
+                 $IPAddress  = $Network.IpAddress[0]                       
+                                         }
+    $MACAddress  = $Network.MACAddress
+	Foreach ($CPU in $CPUInfo)
+	{
+		$infoObject = New-Object PSObject
+		#The following add data to the infoObjects.	
+		Add-Member -inputObject $infoObject -memberType NoteProperty -name "ServerName" -value $CPU.SystemName
+		Add-Member -inputObject $infoObject -memberType NoteProperty -name "PhysicalCores" -value $CPU.NumberOfCores
+		Add-Member -inputObject $infoObject -memberType NoteProperty -name "LogicalCores" -value $CPU.NumberOfLogicalProcessors
+		Add-Member -inputObject $infoObject -memberType NoteProperty -name "Physical_Memory_GB" -value $PhysicalMemory
+		Add-Member -inputObject $infoObject -memberType NoteProperty -name "PageFile_MB" -value $Pagefilesize
+        Add-Member -inputObject $infoObject -memberType NoteProperty -name "IP_Address" -value $IPAddress
+        Add-Member -inputObject $infoObject -memberType NoteProperty -name "MAC_Address" -value $MACAddress
+        Add-Member -inputObject $infoObject -memberType NoteProperty -name "Processor" -value $CPU.Name
+		Add-Member -inputObject $infoObject -memberType NoteProperty -name "OS_Name" -value $OSInfo.Caption		
+        #Add-Member -inputObject $infoObject -memberType NoteProperty -name "TotalVisable_Memory_MB" -value $OSTotalVisibleMemory
+		#Add-Member -inputObject $infoObject -memberType NoteProperty -name "Model" -value $CPU.Description
+		#Add-Member -inputObject $infoObject -memberType NoteProperty -name "Manufacturer" -value $CPU.Manufacturer
+		#Add-Member -inputObject $infoObject -memberType NoteProperty -name "TotalVirtual_Memory_MB" -value $OSTotalVirtualMemory
+		#Add-Member -inputObject $infoObject -memberType NoteProperty -name "CPU_L2CacheSize" -value $CPU.L2CacheSize
+		#Add-Member -inputObject $infoObject -memberType NoteProperty -name "CPU_L3CacheSize" -value $CPU.L3CacheSize
+		#Add-Member -inputObject $infoObject -memberType NoteProperty -name "Sockets" -value $CPU.SocketDesignation
+        #Add-Member -inputObject $infoObject -memberType NoteProperty -name "OS_Version" -value $OSInfo.Version
+		$infoObject #Output to the screen for a visual feedback.
+		$infoColl += $infoObject
+	}
+}
+$infoColl | Export-Csv -path C:\git\suresh-test-git\Server_Inventory_$((Get-Date).ToString('MM-dd-yyyy')).csv -NoTypeInformation #Export the results in csv file.
