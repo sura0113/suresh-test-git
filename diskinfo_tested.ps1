e87be8d2f3b84f18c3b6831ef09ac25cbc2c7654
sura0113 <sura0113@gmail.com>
2017-08-08 20:10:18 +1000

    diskinfo script

---
+++ diskinfo_tested.ps1
@@ -0,0 +1,47 @@
+﻿<#
+*** THIS SCRIPT IS PROVIDED WITHOUT WARRANTY, USE AT YOUR OWN RISK ***
+.DESCRIPTION
+    Use the win32_LogicalDisk WMI Class to get Local Disk Information for one
+    or multiple computers. 
+    Information gathered: 
+        System Name    DeviceID   Volume Name  Size(GB)   FreeSpace(GB)  % FreeSpace(GB)   Date
+
+    Output options include Out-Gridview, a Table, CSV file and an HTML file.
+
+.NOTES
+	File Name: 
+	Author: David Hall
+	Contact Info: 
+		Website: www.signalwarrant.com
+		Twitter: @signalwarrant
+		Facebook: facebook.com/signalwarrant/
+		Google +: plus.google.com/113307879414407675617
+		YouTube Subscribe link: https://www.youtube.com/c/SignalWarrant1?sub_confirmation=1
+	Requires:  
+	Tested: Windows 10 (PS v5), Windows Server 2012R2 (PS v4)
+
+.PARAMETER None
+ 
+.EXAMPLE 
+
+.INFORMATION
+    For a list of Drive Types for the Win32_LogicalDisk class visit the link below
+    https://technet.microsoft.com/en-us/library/ee176674.aspx
+#>
+
+$exportPath = "C:\git\suresh-test-git\" # I change this to a central fileshare
+
+# Your computers.txt will need to be in this folder.
+#$computers = Get-Content "C:\scripts\drive_info\computers.txt"
+
+$driveinfo = Get-WMIobject win32_LogicalDisk -ComputerName . -filter "DriveType=3" |
+                Select-Object SystemName, DeviceID, VolumeName,
+                @{Name="Size(GB)"; Expression={"{0:N1}" -f($_.size/1gb)}},
+                @{Name="FreeSpace(GB)"; Expression={"{0:N1}" -f($_.freespace/1gb)}},
+                @{Name="% FreeSpace(GB)"; Expression={"{0:N2}%" -f(($_.freespace/$_.size)*100)}},
+                @{Name="Date"; Expression={$(Get-Date -format 'g')}} 
+
+# Various Output Options
+#$driveinfo | Out-GridView 
+$driveinfo | Format-Table -AutoSize
+$driveinfo | Export-Csv "$exportPath\diskinfo.csv" -NoTypeInformation -NoClobber -Append
